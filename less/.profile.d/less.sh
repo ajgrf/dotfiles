a7ff48e3d2e4d5e9e7cf6acb77ca07b640941575
Alex Griffin <a@ajgrf.com>
2017-02-08 16:27:56 -0600

    Fix less profile fragment

--- less/.profile.d/less.sh
+++ less/.profile.d/less.sh
@@ -1,12 +1,12 @@
 # make less more friendly for non-text input files, see lesspipe(1)
-hash lesspipe 2> /dev/null && eval "$(SHELL=/bin/sh lesspipe)"
+type lesspipe >/dev/null 2>&1 && eval "$(SHELL=/bin/sh lesspipe)"
 # disable less history file
 export LESSHISTFILE=/dev/null
 export LESS="--ignore-case --no-init --quit-if-one-screen --RAW-CONTROL-CHARS "
 
 # Use Colemak bindings or not
-if test -n "$COLEMAK" -a ! -r "$HOME/.lesskey"; then
+if test -n "$COLEMAK" -a ! -r "$HOME/.less"; then
 	lesskey
-elif test -w "$HOME/.lesskey"; then
-	rm "$HOME/.lesskey"
+elif test -z "$COLEMAK" -a -w "$HOME/.less"; then
+	rm "$HOME/.less"
 fi
