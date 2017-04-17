fc3b3b2464bf4b3732f8c7490e9f26fb07cc943b
Alex Griffin <a@ajgrf.com>
2017-04-17 13:22:30 -0500

    Update Emacs font, layers

--- emacs/.config/spacemacs/init.el
+++ emacs/.config/spacemacs/init.el
@@ -59,6 +59,7 @@ values."
       spell-checking
       syntax-checking
       version-control
+      vimscript
       )
     (if (equal (getenv "COLEMAK") "1")
         '((keyboard-layout :variables
@@ -145,8 +146,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
+   dotspacemacs-default-font '("Go Mono"
+                               ;; :size 13
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
