90b569cd1abe272f4a27066681a8c01ccc828290
Alex Griffin <a@ajgrf.com>
2018-10-14 10:26:28 -0500

    Add nix config (again)

---
+++ nix/config.nix
@@ -0,0 +1,91 @@
+with import <nixpkgs> {};
+
+{
+  firefox.enableBrowserpass = true;
+
+  packageOverrides = pkgs: rec {
+
+    common = with pkgs; buildEnv {
+      name = "common";
+      extraOutputsToInstall = [ "doc" "man" ];
+      paths = [
+        anki
+        gnuapl
+        gopass
+        hledger
+        hugo
+        mblaze
+        neovim
+        newsboat
+        rclone
+        restic
+        ripgrep
+        shfmt
+        stack
+        trash-cli
+        youtube-dl
+      ];
+    };
+
+    nixosProfile = with pkgs; buildEnv {
+      name = "nixosProfile";
+      extraOutputsToInstall = [ "doc" "man" ];
+      paths = [
+        common
+        brightnessctl
+        chromium
+        cron
+        dmenu
+        entr
+        feh
+        ffmpeg
+        file
+        firefox-esr-60
+        go-font
+        gitAndTools.gitFull
+        gnome3.gnome-mines
+        gnome-mpv
+        gnupg
+        go
+        isync
+        jq
+        ledger
+        libreoffice
+        moreutils
+        mpc_cli
+        mpd
+        mpdris2
+        mpv
+        msmtp
+        mutt
+        ncmpc
+        pamix
+        pamixer
+        pandoc
+        (pass.withExtensions (ext: with ext; [ pass-otp pass-import ]))
+        poppler_utils
+        qtpass
+        #quodlibet
+        rxvt_unicode-with-plugins
+        stow
+        syncthing
+        tmux
+        transmission_gtk
+        vim
+        w3m
+        xclip
+        xdotool
+        zathura
+      ];
+    };
+
+    debianProfile = with pkgs; buildEnv {
+      name = "debianProfile";
+      extraOutputsToInstall = [ "doc" "man" ];
+      paths = [
+        common
+        nix
+      ];
+    };
+  };
+}
