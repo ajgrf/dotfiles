74c1392c7ae7cfe31c05a929048b16c95ce5bb0c
Alex Griffin <a@ajgrf.com>
2019-05-25 14:37:10 -0500

    guix: Update GRUB menu-entries

--- .config/guix/system.scm
+++ .config/guix/system.scm
@@ -22,19 +22,19 @@
      (list (menu-entry
             (label "Debian 10 (buster)")
             (device "debboot")
-            (linux "(hd0,gpt3)/vmlinuz-4.19.0-4-amd64")
+            (linux "(hd0,gpt3)/vmlinuz-4.19.0-5-amd64")
             (linux-arguments
              '("root=UUID=227c5e05-6dff-4802-9537-688e20892cf6"
                "ro" "quiet" "splash"))
-            (initrd "(hd0,gpt3)/initrd.img-4.19.0-4-amd64"))
+            (initrd "(hd0,gpt3)/initrd.img-4.19.0-5-amd64"))
            (menu-entry
             (label "PureOS")
             (device "pureosboot")
-            (linux "(hd0,gpt5)/vmlinuz-4.19.0-4-amd64")
+            (linux "(hd0,gpt5)/vmlinuz-4.19.0-5-amd64")
             (linux-arguments
              '("root=UUID=b315dea0-efc1-48ea-9bb4-f1c3aa7e2ce5"
                "ro" "quiet" "splash"))
-            (initrd "(hd0,gpt5)/initrd.img-4.19.0-4-amd64"))))))
+            (initrd "(hd0,gpt5)/initrd.img-4.19.0-5-amd64"))))))
   (mapped-devices
    (list (mapped-device
           (source (uuid "5abba48a-e3e2-4114-8dfc-d97f2a5ba9ac"))
