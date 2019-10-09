40a110cbb30a08297e351f720a68799249185d9b
Alex Griffin <a@ajgrf.com>
2019-10-09 15:27:01 -0500

    guix: Update GRUB entries

--- .config/guix/system.scm
+++ .config/guix/system.scm
@@ -24,13 +24,13 @@
     (keyboard-layout keyboard-layout)
     (menu-entries
      (list (menu-entry
-            (label "Ubuntu 18.04 LTS (bionic)")
-            (device "ububoot")
-            (linux "(hd0,gpt3)/vmlinuz-5.0.0-31-generic")
+            (label "Debian 10 (buster)")
+            (device "debboot")
+            (linux "(hd0,gpt3)/vmlinuz-5.2.0-0.bpo.2-amd64")
             (linux-arguments
-             '("root=UUID=aefc4d76-2428-476f-b9df-d5c1a9e6a150"
+             '("root=UUID=e0856f22-b7e9-40e7-abd9-75f2f3c11337"
                "ro" "quiet" "splash"))
-            (initrd "(hd0,gpt3)/initrd.img-5.0.0-31-generic"))))))
+            (initrd "(hd0,gpt3)/initrd.img-5.2.0-0.bpo.2-amd64"))))))
   (mapped-devices
    (list (mapped-device
           (source (uuid "5abba48a-e3e2-4114-8dfc-d97f2a5ba9ac"))
