003214fc3edfc31db0a7ea918d4acfdd462b5999
Alex Griffin <a@ajgrf.com>
2019-11-03 13:24:58 -0600

    guix: Use local guix checkout if available

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,6 +1,10 @@
-(cons (channel
+(list (channel
        (name 'nonguix)
        (url (if (file-exists? "/home/ajgrf/src/nonguix")
                 "file:///home/ajgrf/src/nonguix"
                 "https://gitlab.com/nonguix/nonguix")))
-      %default-channels)
+      (channel
+       (name 'guix)
+       (url (if (file-exists? "/home/ajgrf/src/guix")
+                "file:///home/ajgrf/src/guix"
+                "https://git.savannah.gnu.org/git/guix.git"))))
