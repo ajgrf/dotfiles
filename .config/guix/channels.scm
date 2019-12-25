6ffe881b467f3b0a507060bcc1f485dea798f31f
Alex Griffin <a@ajgrf.com>
2019-12-25 11:36:19 -0700

    guix: Add guix-gaming-channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -4,6 +4,11 @@
                 "file:///home/ajgrf/src/nonguix"
                 "https://gitlab.com/nonguix/nonguix")))
       (channel
+       (name 'guix-gaming-games)
+       (url (if (not (file-exists? "/home/ajgrf/src/guix-gaming-games"))
+                "file:///home/ajgrf/src/guix-gaming-games"
+                "https://gitlab.com/guix-gaming-channels/games")))
+      (channel
        (name 'guix)
        (url (if (file-exists? "/home/ajgrf/src/guix")
                 "file:///home/ajgrf/src/guix"
