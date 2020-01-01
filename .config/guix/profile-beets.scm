219a594aeddc91e2b51c082068fe15873485c9fa
Alex Griffin <a@ajgrf.com>
2020-01-01 00:11:43 -0600

    Add beets config again

---
+++ .config/guix/profile-beets.scm
@@ -0,0 +1,22 @@
+(use-modules (guix channels)
+             (guix inferior)
+             (srfi srfi-1))  ; for 'first'
+
+(define channels
+  ;; Old revision with bs1770gain@0.5.2.
+  (list (channel
+         (name 'guix)
+         (url "https://git.savannah.gnu.org/git/guix.git")
+         (commit "30696ea2b27747e047589527572ef39e60555047"))))
+
+(define inferior
+  (inferior-for-channels channels))
+
+(define specs
+  '("beets"
+    "ffmpeg"
+    "imagemagick"))  ; for artresizer
+
+(packages->manifest
+ (cons* (first (lookup-inferior-packages inferior "bs1770gain"))
+        (map (compose list specification->package+output) specs)))
