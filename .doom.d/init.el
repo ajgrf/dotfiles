1ca451ce07e5011ee2e602da9b411c8fdd8d20a1
Alex Griffin <a@ajgrf.com>
2020-12-08 18:06:37 -0600

    Disable spell and pdf modules on Windows

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -83,7 +83,8 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (:if (not IS-WINDOWS)
+        spell)           ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
@@ -100,7 +101,8 @@
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       (:if (not IS-WINDOWS)
+        pdf)             ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
