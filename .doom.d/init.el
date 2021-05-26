dc9f5d037ba477de33f04d1a054380a931b0a83f
Alex Griffin <a@ajgrf.com>
2021-05-26 11:35:26 -0500

    Disable (:tools pdf) on WSL

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -108,7 +108,8 @@
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       (:if (not IS-WINDOWS)
+       (:if (not (or IS-WINDOWS
+                     IS-WSL))
         pdf)             ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
